bf7079189a759522e27df78abb3fb861edc96b11
Samuel Eubanks <swamulism@gmail.com>
2019-11-19 19:50:31 -0700

    Add start to code

--- A4.py
+++ A4.py
@@ -18,23 +18,26 @@ def distort_input(instance, percent_distortion):
     print("TODO")
 
 
-
+#  size of our weight matrix should be 25x25 and not 5x5??????
 class HopfieldNetwork:
 	def __init__(self, size):
 		self.h = np.zeros([size,size])
 
-	def addSinglePattern(p):
+	def addSinglePattern(self, p):
 		#Update the hopfield matrix using the passed pattern
-		print("TODO")
-		utils.raiseNotDefined()
+		for row in range(len(self.h)):
+			for col in range(len(self.h[row])):
+				if row > col:
+					self.h[row][col] += (2 * p[col] - 1) * (2 * p[row] - 1)
+					self.h[col][row] = self.h[row][col]
 
-	def fit(patterns):
+	def fit(self, patterns):
 		# for each pattern
 		# Use your addSinglePattern function to learn the final h matrix
-		print("TODO")
-		utils.raiseNotDefined()
+		for p in patterns:
+			self.addSinglePattern(p)
 
-	def retrieve(input):
+	def retrieve(self, input):
 		#Use your trained hopfield network to retrieve and return a pattern based on the
 		#input pattern.
 		#HopfieldNotes.pdf on canvas is a good reference for asynchronous updating which
@@ -45,7 +48,7 @@ class HopfieldNetwork:
 		print("TODO")
 		utils.raiseNotDefined()
 
-	def classify(inputPattern):
+	def classify(self, inputPattern):
 		#Classify should consider the input and classify as either, five or two
 		#You will call your retrieve function passing the input
 		#Compare the returned pattern to the 'perfect' instances
