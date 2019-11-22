1d6c1ec51b57b9daeef3db7c49adc44e46d1a7e9
samuel <swamulism@gmail.com>
2019-11-22 01:02:34 -0700

    Finish assignment code

--- A4.py
+++ A4.py
@@ -2,6 +2,8 @@ import numpy as np
 import pandas as pd
 import utils
 from sklearn.neural_network import MLPClassifier
+from sklearn.metrics import accuracy_score
+import matplotlib.pyplot as plt
 
 #Perfect Instances
 five =  [0,1,1,1,0, 0,1,0,0,0, 0,1,1,1,0, 0,0,0,1,0, 0,1,1,1,0]
@@ -9,7 +11,7 @@ two = [0,1,1,1,0, 0,0,0,1,0, 0,1,1,1,0, 0,1,0,0,0, 0,1,1,1,0]
 patterns = [five,two]
 
 def loadGeneratedData():
-	print("TODO")
+	return pd.read_csv("saeu4280-TrainingData.csv")
 
 def distort_input(instance, percent_distortion):
     #percent distortion should be a float from 0-1
@@ -85,17 +87,111 @@ class HopfieldNetwork:
 		else:
 			return "unknown"
 
-
-
-if __name__ == "__main__":
+# part 2
+def run_hopfield():
 	hopfieldNet = HopfieldNetwork(25)
-
-	# utils.visualize(five)
-	# utils.visualize(two)
-
-
 	hopfieldNet.fit(patterns)
-
-	df = pd.read_csv("saeu4280-TrainingData.csv")
+	df = loadGeneratedData()
 	for index, row in df.iterrows():
 		print(hopfieldNet.classify(row[:-1]))
+
+# part 3
+def run_MLP():
+	df = loadGeneratedData()
+	nn = MLPClassifier()
+	nn.fit(patterns, ["five", "two"])
+	print(nn.predict(df.loc[:, df.columns != 'label']))
+
+# part 4
+def run_distorted(num_sample):
+	nn = MLPClassifier()
+	nn.fit(patterns, ["five", "two"])
+
+	hopfieldNet = HopfieldNetwork(25)
+	hopfieldNet.fit(patterns)
+
+	y_true = ["five"] * num_sample + ["two"] * num_sample
+	x = np.arange(0, 0.51, 0.01)
+	y_mlp = []
+	y_hopfield = []
+	for i in x:
+		distorted_values = [distort_input(five, i) for _ in range(num_sample)] + [distort_input(two, i) for _ in range(num_sample)]
+		y_pred = nn.predict(distorted_values)
+
+		y_hopfield.append(accuracy_score(y_true, [hopfieldNet.classify(val) for val in distorted_values]))
+		y_mlp.append(accuracy_score(y_true, y_pred))
+
+
+	fig, ax = plt.subplots(figsize=(12, 8))
+	ax.plot(x, y_mlp, label="MLP")
+	ax.plot(x, y_hopfield, label="hopfield")
+	ax.set_title('Distortion vs Accuracy (1000 examples each)')
+	ax.set_ylabel('Accuracy')
+	ax.set_xlabel('Percent Distortion')
+
+	plt.legend()
+
+	ax.yaxis.grid(color='grey', linestyle='-', linewidth=0.25, alpha=0.5)
+	fig.savefig('plot1.png')
+
+# part 5
+def run_hidden_layers(num_sample):
+
+	df1 = pd.read_csv("NewInput.csv")
+	df2 = loadGeneratedData()
+
+	df_comb = df1.append(df2)
+	df_comb.loc[len(df_comb)] = five+["five"]
+	df_comb.loc[len(df_comb)] = two+["two"]
+
+	# for _, row in df.iterrows():
+	# 	utils.visualize(row[:-1])
+
+
+	
+
+	y_true = ["five"] * num_sample + ["two"] * num_sample
+	x = np.arange(0, 0.51, 0.01)
+	
+	fig, ax = plt.subplots(figsize=(12, 8))
+
+	for num_layers in range(1, 22, 2):
+		tmp = (100,) * num_layers
+		nn = MLPClassifier(hidden_layer_sizes=tmp)
+		nn.fit(df_comb.iloc[:,:-1], df_comb.iloc[:,-1])
+		y_mlp = []
+		for i in x:
+			distorted_values = [distort_input(five, i) for _ in range(num_sample)] + [distort_input(two, i) for _ in range(num_sample)]
+			y_pred = nn.predict(distorted_values)
+			y_mlp.append(accuracy_score(y_true, y_pred))
+		ax.plot(x, y_mlp, label=f"{num_layers} Layers")
+
+
+	plt.legend()
+
+	ax.set_title('Distortion vs MLP Accuracy (1000 examples each)')
+	ax.set_ylabel('Accuracy')
+	ax.set_xlabel('Percent Distortion')
+
+	ax.yaxis.grid(color='grey', linestyle='-', linewidth=0.25, alpha=0.5)
+	fig.savefig('plot2.png')
+
+if __name__ == "__main__":
+
+	print("Assignment 4")
+	# part 1
+	# print(loadGeneratedData())
+	
+	# part 2
+	# run_hopfield()
+
+	# part 3
+	# run_MLP()
+	
+	# part 4
+	# run_distorted(10)
+
+	# part 5
+	# run_hidden_layers(1000)
+
+	
